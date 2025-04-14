acb8bc270b34fcb3cc266d0db69f6dfcd986884b
Anirudra628 <whyheisani@gmail.com>
2025-04-14 21:44:28 +0530

    Update productrepoImpl.java

--- src/main/java/com/codeeasy/project/repository/productrepoImpl.java
+++ src/main/java/com/codeeasy/project/repository/productrepoImpl.java
@@ -114,19 +114,23 @@ public class productrepoImpl implements CustomMethod {
                 String doctor_last_name = rs.getString(2);
                 String clinic_name = rs.getString(3);
                 String degree = rs.getString(4);
-                String experience = rs.getString(5);
+                String doctor_id = rs.getString(5);
                 String slot = rs.getString(6);
-                String timing_start = rs.getString(7);
-                String timing_end = rs.getString(8);
-                String fees = rs.getString(9);
+                String location_id = rs.getString(7);
+                String date = rs.getString(8);
+                String timing_start = rs.getString(9);
+                String timing_end = rs.getString(10);
+                String fees = rs.getString(11);
 
                 List<String> inner_list = new ArrayList<>();
                 inner_list.add(doctor_first_name);
                 inner_list.add(doctor_last_name);
                 inner_list.add(clinic_name);
                 inner_list.add(degree);
-                inner_list.add(experience);
+                inner_list.add(doctor_id);
                 inner_list.add(slot);
+                inner_list.add(location_id);
+                inner_list.add(date);
                 inner_list.add(timing_start);
                 inner_list.add(timing_end);
                 inner_list.add(fees);
@@ -145,6 +149,80 @@ public class productrepoImpl implements CustomMethod {
         return doctor_list;
     }
    
+    @Override
+    public Integer save_appointment(String email,String relation, String doctor_id, String location_id, String slot_id, String confirm)
+    {
+        int successcode= 0;
+
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection established successfully!");
+
+            CallableStatement cstmt = conn.prepareCall("{call SAVE_APPOINTMENT(?,?,?,?,?,?,?)}");
+            cstmt.setString(1, email);
+            cstmt.setString(2, relation);
+            cstmt.setString(3, doctor_id);
+            cstmt.setString(4, location_id);
+            cstmt.setString(5, slot_id);
+            cstmt.setString(6, confirm);
+            cstmt.registerOutParameter(7, java.sql.Types.NUMERIC);
+
+            cstmt.execute();
+            successcode = cstmt.getInt(7);
+            System.out.println("ResultSet obtained successfully!");
+
+           /*  while(rs.next()){
+                 successcode = rs.getInt(1);
+
+            }*/
+           // rs.close();
+            cstmt.close();
+            
+        }
+
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+       
+        return successcode;
+    }
+
+    @Override
+    public Integer add_patient(String email, String p_fname, String p_lname, String dob, String sex, String relation) {
+        int successCode = 0;
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection established successfully!");
+
+            CallableStatement cstmt = conn.prepareCall("{call ADDPATIENT(?,?,?,?,?,?,?)}");
+            cstmt.setString(1, email);
+            cstmt.setString(2, p_fname);
+            cstmt.setString(3, p_lname);
+            cstmt.setString(4, dob);
+            cstmt.setString(5, sex);
+            cstmt.setString(6, relation);
+            cstmt.registerOutParameter(7, java.sql.Types.NUMERIC);
+
+            cstmt.execute();
+            successCode = cstmt.getInt(7);
+
+            if (successCode == 0)
+                System.out.println("Patient added successfully!");
+
+            cstmt.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return successCode;
+    }
 
 }
+
 
