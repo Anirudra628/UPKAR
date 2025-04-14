3e909196e149ae680de5d7bc3b4e44f11e3279e1
Anirudra628 <whyheisani@gmail.com>
2025-04-14 21:43:50 +0530

    Update CustomMethod.java

--- src/main/java/com/codeeasy/project/repository/CustomMethod.java
+++ src/main/java/com/codeeasy/project/repository/CustomMethod.java
@@ -10,6 +10,9 @@ public interface CustomMethod {
 
     List<List<String>> getappointmentId(String email); // Integer is the primary key datatype
     List<List<String>> search_doctors(String location, String specialization, String clinic_type,String day);
+    Integer save_appointment(String email,String relation, String doctor_id, String location_id, String slot_id,String confirm);
+
+    Integer add_patient(String email, String p_fname, String p_lname, String dob, String sex, String relation);
     
     
 }
